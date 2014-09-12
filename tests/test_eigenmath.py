4994e6bd8ff29f1ebafd880110f9519b962fb22b
Tim Lewandowski <tlewandowski@luminoso.com>
2014-09-12 15:57:15 -0400

    Add test for decompositions of different dimensions

--- tests/test_eigenmath.py
+++ tests/test_eigenmath.py
@@ -166,6 +166,17 @@ def test_combine_multiple_eigenspaces():
     assert np.allclose(s_c, s_ref)
     compare_cols_within_sign(u_c, u_ref)
 
+    # Check to make sure that the results agree with combine_eigenspaces
+    # when n = 2 and dim(U_0) != dim(U_1)
+
+    u4 = np.asarray([[0, 0, 0], [1, 0, 0], [0, 0, 0], [0, 1, 0], [0, 0, 1]]) 
+    s4 = np.asarray([1, 0.5, 0.4])
+
+    u_c, s_c = combine_multiple_eigenspaces([(u1, s1), (u4, s4)], 3)
+    u_ref, s_ref = combine_eigenspaces(u1, s1, u4, s4, 3)
+    assert np.allclose(s_c, s_ref)
+    compare_cols_within_sign(u_c, u_ref)
+
     # Check to make sure the results agree with redecompose when n = 1
     # This is matrix #1 from the previous test, but with a row missing.
     u_in = np.asarray([[0.5, 0.5],
