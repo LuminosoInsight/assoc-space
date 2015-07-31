df689719e80cd9f1d5922f0e1916a5afcb4ebea5
Rob Speer <rob@luminoso.com>
2015-07-31 11:15:46 -0400

    call this 1.0.2 because PyPI is unforgiving

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="assoc_space",
-    version='1.0.1',
+    version='1.0.2',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='dev@luminoso.com',
     license="MIT",
