a1355184a1c7e2430d52693bdc17fc598d22a757
Rob Speer <rob@luminoso.com>
2015-07-31 11:11:21 -0400

    apparently 1.0 was released, call it 1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="assoc_space",
-    version='1.0.0',
+    version='1.0.1',
     maintainer='Luminoso Technologies, Inc.',
     maintainer_email='dev@luminoso.com',
     license="MIT",
